0b42f3681a2e23c7f2adf3892246a89ee364aa87
Florent Moiny <florent.moiny@gmail.com>
2016-08-21 19:04:48 +0200

    linked list: improved reverse print with recursive function

--- data_structures/linked_lists/print_reverse.py
+++ data_structures/linked_lists/print_reverse.py
@@ -14,3 +14,11 @@ def reverse_print(head):
     for i in range(len(data)):
         print(data[len(data)-i-1])
 
+# better solution with recursive function
+
+def reverse_print(head):
+    if not head:
+        return
+    else:
+        reverse_print(head.next)
+    print head.data
