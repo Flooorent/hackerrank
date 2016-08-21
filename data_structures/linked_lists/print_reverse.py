793c016904dd00e15a6fc7152f6e595593c1c274
Florent Moiny <florent.moiny@gmail.com>
2016-08-21 19:26:23 +0200

    linked list: added missing default parameters

--- data_structures/linked_lists/print_reverse.py
+++ data_structures/linked_lists/print_reverse.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 class Node(object):
-    def __init__(self, data, next_node):
+    def __init__(self, data=None, next_node=None):
         self.data = data
         self.next = next_node
 
